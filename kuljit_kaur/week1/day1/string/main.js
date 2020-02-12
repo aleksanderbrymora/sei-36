defbab8bac256205e0662dcbefbcfd509bfe57c3
Kuljit Kaur <kuljitkaur@Kuljits-MacBook-Air.local>
2020-02-12 16:53:51 +1100

    String Assignment finished

--- kuljit_kaur/week1/day1/string/main.js
+++ kuljit_kaur/week1/day1/string/main.js
@@ -10,8 +10,7 @@
 //
 //   DrEvil(10): 10 dollars
 //   DrEvil(1000000): 1000000 dollars (pinky)
-  console.log('Please enter amount for DrEvil');
-  const DrEvil = function(amount)
+  const drEvil = function(amount)
   {
   if(amount === 1000000)
   {
@@ -22,6 +21,9 @@
   return `${amount} dollars`;
   }
 }
+console.log(`drEvil for 10 is ${drEvil(10)}`);
+console.log(`drEvil for 1000000 is ${drEvil(1000000)}`);
+
 // MixUp
 // Create a function called mixUp. It should take in two strings, and return the
  // concatenation of the two strings (separated by a space) slicing out and swapping
@@ -31,7 +33,7 @@
 //   mixUp('mix', 'pod'): 'pox mid'
 //   mixUp('dog', 'dinner'): 'dig donner'
 
-const mixUp = function( str1 , str2 )
+const mixUp = function( str1 , str2)
 {
 var part1 = str1.slice(0,2),
     part2 = str2.slice(0,2),
@@ -39,7 +41,6 @@ var part1 = str1.slice(0,2),
     newstr2 = str2.replace(part2, part1);
     console.log(`Changed String are: ${newstr1} and ${newstr2} ` );
 }
-
 
 
 
@@ -56,11 +57,10 @@ const fixStart = function(string1)
 {
   var str = string1.slice(1,string1.length);
   var alphabet = string1[0];
-  let string2,
-      charac,
-      length = str.length - 1;
-  for(i=0; i<= length; i+=1 )
-  { if (str[i] === alphabet)
+  var string2 = '',
+      charac;
+  for(i=0; i< str.length; i+=1 )
+  { if (str[i] == alphabet)
     {
       string2 += "*";
     }
@@ -70,10 +70,11 @@ const fixStart = function(string1)
       string2 += charac;
     }
   }
-  console.log(string2);
   string2 = alphabet + string2;
+  // console.log(`New String :" ${string2}`);
   return string2;
 }
+console.log(`fixStart('babble'): ${fixStart('babble')}`);
 
 // Verbing
 // Create a function called verbing. It should take a single argument, a string.
@@ -86,14 +87,60 @@ const fixStart = function(string1)
 //   verbing('go'): 'go'
 // Not Bad
 
+const verbing = function(str)
+{
+  var str1 = str.substr(-3);
+if (str.length >=3 )
+  {
+    if ( str1 === 'ing' )
+    { str = str + 'ly';
+      return str;
+    }
+    else
+    {
+    str = str + "ing";
+    return str;
+    }
+   }
+ else
+    return str;
+  }
+console.log(`verbing('swim'):  ${verbing('swim')}`);
+console.log(`verbing('swimming'):  ${verbing('swimming')}`);
+console.log(`verbing('go'):  ${verbing('go')}`);
 
 // Create a function called notBad that takes a single argument, a string.
 //
 // It should find the first appearance of the substring 'not' and 'bad'.
-// If the 'bad' follows the 'not', then it should replace the whole 'not'...'bad' substring with 'good' and return the result.
-// If it doesn't find 'not' and 'bad' in the right sequence (or at all), just return the original sentence.
+// If the 'bad' follows the 'not', then it should replace the whole 'not'...'bad'
+ // substring with 'good' and return the result.
+// If it doesn't find 'not' and 'bad' in the right sequence (or at all), just
+// return the original sentence.
 // For example:
 //
 //   notBad('This dinner is not that bad!'): 'This dinner is good!'
   // notBad('This movie is not so bad!'): 'This movie is good!'
   // notBad('This dinner is bad!'): 'This dinner is bad!'
+
+const notBad = function (string1)
+{
+  var indexNot = string1.indexOf("not");
+  var sub , sublength;
+  if (indexNot != -1)
+  {
+  subindex = indexNot + 1;
+  sub = string1.substr(subindex,string1.length);
+  if (sub.indexOf("bad"))
+  {
+    string1 = string1.slice(string1[0],indexNot - 1);
+    return (string1 = string1 + " good!");
+  }
+  }
+  else {
+    return string1;
+  }
+}
+
+console.log( `This dinner is not that bad! is converted to ${notBad('This dinner is not that bad!')} `);
+console.log( `This movie is not so bad! is converted to ${notBad('This movie is not so bad!')} `);
+console.log( `This dinner is bad! is converted to ${notBad('This dinner is bad!')} `);

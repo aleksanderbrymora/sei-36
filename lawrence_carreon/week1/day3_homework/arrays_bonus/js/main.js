93b1def62ca9f7d1aa94f6040dd4bd9c1d9bf662
Lawrence Carreon <l.carreon123@gmail.com>
2020-02-12 17:40:17 +1100

    array bonus and word guesser complete

---
+++ lawrence_carreon/week1/day3_homework/arrays_bonus/js/main.js
@@ -0,0 +1,27 @@
+const maxOfTwoNumbers = function(num1, num2){
+    return num1 < num2 ? num2 : num1;
+};
+
+const maxOfThreeNumbers = function(num1, num2, num3){
+    return maxOfTwoNumbers( maxOfTwoNumbers(num1,num2) , num3 ); 
+};
+
+const sumArray = function(array){
+    return array.reduce(function(a,b){return a + b});
+};
+
+const multiplyArray = function(array){
+    return array.reduce(function(a,b){return a * b});
+};
+
+const reverseString = function(string){
+    return string.split("").reverse().join("");
+};
+
+const findLongestWord = function(wordArray){
+    return wordArray.sort(function(a,b){return a.length - b.length}).pop();
+};
+
+const filterLongWord = function(wordArray, lengthCap){
+    return wordArray.filter(element => element.length > lengthCap );
+};

1d3d2edf1a79fbab1a6b5570a2e967a6bd51ccd7
nlmiv <napmanaog@aim.com>
2020-02-14 09:30:12 +1100

    pull 14Feb2020 09:29

---
+++ napoleon_manaog/week_01/hw/d4/hw-w1d4.js
@@ -0,0 +1,128 @@
+// Part 1, Rectangle
+const rectangleA = {
+  length: 4,
+  width: 4
+};
+
+const isSquare = function (obj){
+  let checkSquare = false;
+  if (obj.length === obj.width) {
+      checkSquare = true;
+  }
+  return checkSquare;
+};
+
+const areaRectangle = function (obj) {
+  return obj.length * obj.width;
+};
+
+const perimeterRectangle = function (obj) {
+  return (2 * obj.length) + (2 * obj.width);
+};
+
+console.log(isSquare(rectangleA));
+console.log(areaRectangle(rectangleA));
+
+
+// Part 2, Triangle
+const triangleA = {
+  sideA: 3,
+  sideB: 4,
+  sideC: 4
+};
+
+const isEquilateral = function (obj) {
+  let allSidesEqual = false;
+  if (obj.sideA === obj.sideB && obj.sideB === obj.sideC && obj.sideC === obj.sideA) {
+    sidesEqual = true;
+  }
+  return allSidesEqual;
+}
+
+const isIsosceles = function (obj) {
+  let twoSidesEqual = false;
+  if (obj.sideA === obj.sideB) {
+    twoSidesEqual = true;
+  }
+  if (obj.sideB === obj.sideC) {
+    twoSidesEqual = true;
+  }
+  if (obj.sideC === obj.sideA) {
+    twoSidesEqual = true;
+  }
+  return twoSidesEqual;
+}
+
+const isObtuse = function (o) {
+  let isObt = false;
+  if (((o.sideA*o.sideA)+(o.sideB*o.sideB)) < (o.sideC*o.sideC)) {
+    isObt = true;
+  }
+  return isObt;
+}
+
+const areaTriangle = function (obj) {
+  return((obj.sideB*obj.sideC)/2);
+}
+
+// The Cash Register
+// Input
+const cartForParty = {
+  banana: "1.25",
+  handkerchief: ".99",
+  Tshirt: "25.01",
+  apple: "0.60",
+  nalgene: "10.34",
+  proteinShake: "22.36"
+};
+const cashRegister = function (o) {
+  let oArray = Object.values(o);
+  let total = 0;
+  for (var i = 0; i < oArray.length; i++) {
+    total += Number(oArray[i]);
+  }
+  // console.log(total);
+  return total;
+}
+
+// Output
+console.log(`Total: ${cashRegister(cartForParty)}`);
+
+
+// JavaScript Bank
+const jsBanks = {
+  accounts: [
+    { acctId: 0, name: "Mary",  balance: 100.00 },
+    { acctId: 1, name: "Peter", balance: 199.00 },
+    { acctId: 2, name: "John",  balance: 1200.00 }
+  ],
+  getBalance: function (oAcct) {
+    return (oAcct.balance);
+  },
+  deposit: function (oAcct, amount) {
+    return (oAcct.balance += amount);
+  },
+  withdraw: function (oAcct, amount) {
+    let success = false;
+    if( amount <= oAcct.balacne) {
+      oAcct.balance -= amount
+      success = true;
+    }
+    return success;
+  },
+  transfer: (oSource, oTarget, amount){
+    let success = false;
+    if(this.withdraw(oSource,amount) {
+        this.deposit(oTarget,amount);
+        success = true;
+    }
+    return success;
+  },
+  totalBankBalance: function (){
+    let total = 0;
+    for (var i = 0; i < this.accounts.length; i++) {
+      total += this.accounts[i].balance;
+    }
+    return total;
+  }
+}

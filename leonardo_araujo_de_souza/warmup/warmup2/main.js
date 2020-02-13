e6781b90cd5e369abca5015873b1a82cd3784c7f
leonardo souza <leo.aus88@gmail.com>
2020-02-13 11:58:36 +1100

    homework2

---
+++ leonardo_araujo_de_souza/warmup/warmup2/main.js
@@ -0,0 +1,32 @@
+
+
+// # Filter
+
+
+// 1. Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+// ## Example:
+
+// Input:
+
+// 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+// Output:
+
+// 'alpha beta gamma delta'
+
+// # Bonus question
+
+// Create a function that returns a string 'banana'. The catch is you can only use letters 'a' and 'b'.
+// Super extra bonus points for proving "how well engineered Javascript really is".
+const filterWords = function (str) {
+  const words = str.split(' ');
+  const Output = [];
+
+  for (let i = 0; i < words.length; i++){
+
+  }
+
+}
+
+filterwords(alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta)

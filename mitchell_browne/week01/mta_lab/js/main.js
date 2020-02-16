f7f5c510bc3f0bd01f615e6060d079b576660a69
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-02-16 20:53:07 +1100

    Week01 Day 5 MTA refined code

--- mitchell_browne/week01/mta_lab/js/main.js
+++ mitchell_browne/week01/mta_lab/js/main.js
@@ -1,14 +1,14 @@
-// add train trip factory to store each trip plan into an object and grab and store values from/in the object
-
+// object trainNetwork
 const trainNetwork = {
 
   // factory to add further lines
-  lineFactory: function(name, stops) { // name is key, stops is array
+  addLine: function(name, stops) { // name is key, stops is array
     this.lines[name] = stops;
+    console.log(`Added ${name} line: ${this.lines[name]}.`)
   },
 
   // factory to add trip details
-  tripFactory: function(lineFrom, start, lineTo, end) {
+  addTrip: function(lineFrom, start, lineTo, end) {
     let tripNumber = Object.keys(this.trips).length+1;
     this.trips[tripNumber] = {lineFrom: lineFrom, start: start, lineTo: lineTo, end: end};
   },
@@ -22,143 +22,167 @@ const trainNetwork = {
 
   // object to hold details of each trip
   trips: {
-    // 0: {lineFrom: , start: , lineTo: , end: }
+    // 0: {lineFrom: 'N', start: '34th', lineTo: 'L', end: '1st', lineExists: 'true', stationExists: 'true',
+    // sameLine: 'false', tripOneArray: ["28th", "23rd", "Union Square"], tripTwoArray: ["3rd", "1st"], stops: 5}
   },
 
   tripCount: {
     trip: 0
   },
 
+  printTrips: function() {
+    for (const trip in this.trips) {
+      if (this.trips[trip].lineExists === false || this.trips[trip].stationExists === false) {
+        console.log(`${trip}: ${this.trips[trip].start} (${this.trips[trip].lineFrom}) \
+to ${this.trips[trip].end} (${this.trips[trip].lineTo}) could not be completed.`);
+      } else {
+      console.log(`${trip}: ${this.trips[trip].start} (${this.trips[trip].lineFrom}) \
+to ${this.trips[trip].end} (${this.trips[trip].lineTo}) with ${this.trips[trip].stops} stops.`);
+      }
+    }
+  },
+
+  printLines: function() {
+    for (let i = 0; i < Object.keys(this.lines).length; i++) {
+      let key = Object.keys(this.lines)[i];
+      console.log(`${key}: ${this.lines[key].join(', ')}`);
+    }
+  },
+
   // function to run the methods to plan the trip
   planTrip: function(lineFrom, start, lineTo, end) {
-    this.tripFactory(lineFrom, start, lineTo, end);
+    this.addTrip(lineFrom, start, lineTo, end);
     this.tripCount.trip = Object.keys(this.trips).length;
-    this.inputMessage();
-    if (!this.lineExists(lineFrom, lineTo)) {
+    let count = this.tripCount.trip;
+    this.inputMessage(count);
+    if (!this.lineExists(count)) {
       return;
     };
     // test if station provided exists
-    if (!this.stationExists(lineFrom, start, lineTo, end)) {
+    if (!this.stationExists(count)) {
       return;
     };
     // test if both stations are the same line
-    if (this.compareLine(lineFrom, lineTo)) {
-      const tripSameLineArray = this.tripSameLine(lineFrom, start, end);
-      this.tripSameLineMessage(lineFrom, tripSameLineArray);
+    if (this.compareLine(count)) {
+      this.tripSameLine(count);
+      this.tripSameLineMessage(count);
       return;
     };
     // returns the stops on each line to catch
-    const tripOneArray = this.tripOne(lineFrom, start);
-    const tripTwoArray = this.tripTwo(lineTo, end);
-    this.tripMessage(lineFrom, tripOneArray, lineTo, tripTwoArray);
+    this.tripOne(count);
+    this.tripTwo(count);
+    this.tripMessage(count);
   },
 
-  // test if each station is on the same line
-  compareLine: function(lineFrom, lineTo) {
-    if (lineFrom === lineTo) {
-      return true;
-    };
-    return false;
-  },
-
-  lineExists: function(lineFrom, lineTo) {
-    if (Object.keys(this.lines).indexOf(lineFrom) < 0) {
-      console.log(`${lineFrom} line does not exist on this network.`);
-      return false;
-    } else if (Object.keys(this.lines).indexOf(lineTo) < 0) {
-      console.log(`${lineTo} line does not exist on this network.`);
-      return false;
+  lineExists: function(count) {
+    if (Object.keys(this.lines).indexOf(this.trips[count].lineFrom) < 0) {
+      console.log(`${this.trips[count].lineFrom} line does not exist on this network.`);
+      return this.trips[count].lineExists = false;
+    } else if (Object.keys(this.lines).indexOf(this.trips[count].lineTo) < 0) {
+      console.log(`${this.trips[count].lineTo} line does not exist on this network.`);
+      return this.trips[count].lineExists = false;
     }
-    return true;
+    return this.trips[count].lineExists = true;
   },
 
   // tests if station input exists on the input line
-  stationExists: function(lineFrom, start, lineTo, end) {
-    if (this.lines[lineFrom].indexOf(start) < 0) {
-      console.log(`${start} station does not exist on the ${lineFrom} line.`)
-      return false;
-    } else if (this.lines[lineTo].indexOf(end) < 0) {
-      console.log(`${end} station does not exist on the ${lineTo} line.`)
-      return false;
+  stationExists: function(count) {
+    if (this.lines[this.trips[count].lineFrom].indexOf(this.trips[count].start) < 0) {
+      console.log(`${this.trips[count].start} station does not exist on the ${this.trips[count].lineFrom} line.`)
+      return this.trips[count].stationExists = false;
+    } else if (this.lines[this.trips[count].lineTo].indexOf(this.trips[count].end) < 0) {
+      console.log(`${this.trips[count].end} station does not exist on the ${this.trips[count].lineTo} line.`)
+      return this.trips[count].stationExists = false;
     }
-    return true;
+    return this.trips[count].stationExists = true;
+  },
+
+  // test if each station is on the same line
+  compareLine: function(count) {
+    if (this.trips[count].lineFrom === this.trips[count].lineTo) {
+      return this.trips[count].sameLine = true;
+    };
+    return this.trips[count].sameLine = false;
   },
 
   // finds each station stop and adds to an array to return
-  tripSameLine: function(lineFrom, start, end) {
-    const tripSameLineArray = [];
-    const firstStationIndex = this.lines[lineFrom].indexOf(start);
-    const lastStationIndex = this.lines[lineFrom].indexOf(end);
+  tripSameLine: function(count) {
+    this.trips[count].tripSameLineArray = [];
+    const firstStationIndex = this.lines[this.trips[count].lineFrom].indexOf(this.trips[count].start);
+    const lastStationIndex = this.lines[this.trips[count].lineFrom].indexOf(this.trips[count].end);
 
     if (firstStationIndex < lastStationIndex) {
       for (let i = firstStationIndex + 1; i <= lastStationIndex; i++) {
-        tripSameLineArray.push(this.lines[lineFrom][i]);
+        this.trips[count].tripSameLineArray.push(this.lines[this.trips[count].lineFrom][i]);
       }
     } else {
       for (let i = firstStationIndex + 1; i >= lastStationIndex; i--) {
-        tripSameLineArray.push(this.lines[lineFrom][i]);
+        this.trips[count].tripSameLineArray.push(this.lines[this.trips[count].lineFrom][i]);
       }
     }
-    return tripSameLineArray;
+    this.trips[count].stops = this.trips[count].tripSameLineArray.length;
   },
 
   // finds each station stop from start station to union square, returns an array
-  tripOne: function(lineFrom, start) {
-    const tripOneArray = [];
-    const firstLine = this.lines[lineFrom];
-    const firstStationIndex = firstLine.indexOf(start);
+  tripOne: function(count) {
+    this.trips[count].tripOneArray = [];
+    const firstLine = this.lines[this.trips[count].lineFrom];
+    const firstStationIndex = firstLine.indexOf(this.trips[count].start);
     const toUnionSquareIndex = firstLine.indexOf('Union Square');
     if (firstStationIndex < toUnionSquareIndex) {
       for (let i = firstStationIndex + 1; i <= toUnionSquareIndex; i++) {
-        tripOneArray.push(firstLine[i]);
+        this.trips[count].tripOneArray.push(firstLine[i]);
       }
     } else {
-      for (let i = firstStationIndex + 1; i >= toUnionSquareIndex; i--) {
-        tripOneArray.push(firstLine[i]);
+      for (let i = firstStationIndex - 1; i >= toUnionSquareIndex; i--) {
+        this.trips[count].tripOneArray.push(firstLine[i]);
       }
     }
-    return tripOneArray;
   },
 
   // finds each station from union square to final station, returns an array
-  tripTwo: function(lineTo, end) {
-    const tripTwoArray = [];
-    const secondLine = this.lines[lineTo];
+  tripTwo: function(count) {
+    this.trips[count].tripTwoArray = [];
+    const secondLine = this.lines[this.trips[count].lineTo];
     const fromUnionSquareIndex = secondLine.indexOf('Union Square');
-    const lastStationIndex = secondLine.indexOf(end);
+    const lastStationIndex = secondLine.indexOf(this.trips[count].end);
     if (lastStationIndex < fromUnionSquareIndex) {
-      for (let i = fromUnionSquareIndex-1; i >= lastStationIndex; i--) {
-        tripTwoArray.push(secondLine[i]);
+      for (let i = fromUnionSquareIndex - 1; i >= lastStationIndex; i--) {
+        this.trips[count].tripTwoArray.push(secondLine[i]);
       }
     } else {
-      for (let i = fromUnionSquareIndex; i <= lastStationIndex; i++) {
-        tripTwoArray.push(secondLine[i]);
+      for (let i = fromUnionSquareIndex + 1; i <= lastStationIndex; i++) {
+        this.trips[count].tripTwoArray.push(secondLine[i]);
       }
     }
-    return tripTwoArray;
+    this.trips[count].stops = (this.trips[count].tripOneArray.length + this.trips[count].tripTwoArray.length);
   },
 
   // logs the details inputed by user
-  inputMessage: function(lineFrom, start, lineTo, end) {
-    console.log(`----- TRIP ${this.tripCount.trip}: ${this.trips[this.tripCount.trip].start} (${lineFrom}) to ${end} (${lineTo}) -----`);
-  },
-
-  // logs the trip details to console for user
-  tripMessage: function(lineFrom, tripOneArray, lineTo, tripTwoArray) {
-    console.log(`You must travel through the following stops on the ${lineFrom} line: ${tripOneArray.join(', ')}.`);
-    console.log(`Change at Union Square to the ${lineTo} line.`);
-    console.log(`Your journey continues through the following stops: ${tripTwoArray.join(', ')}.`);
-    console.log(`${tripOneArray.length+tripTwoArray.length} stops in total.`);
+  inputMessage: function(count) {
+    console.log(`----- TRIP ${count}: ${this.trips[count].start} (${this.trips[count].lineFrom}) \
+to ${this.trips[count].end} (${this.trips[count].lineTo}) -----`);
   },
 
   // logs the same line trip details to console for user
-  tripSameLineMessage: function(line, tripSameLineArray) {
-    console.log(`You must travel through the following stops on the ${line} line: ${tripSameLineArray.join(', ')}.`);
+  tripSameLineMessage: function(count) {
+    console.log(`You must travel through the following stops on the ${this.trips[count].lineFrom} line: \
+${this.trips[count].tripSameLineArray.join(', ')}.`);
     console.log(`No change.`);
-    console.log(`${tripSameLineArray.length} stops in total.`);
+    console.log(`${this.trips[count].stops} stops in total.`);
+  },
+
+  // logs the trip details to console for user
+  tripMessage: function(count) {
+    console.log(`You must travel through the following stops on the ${this.trips[count].lineFrom} line: \
+${this.trips[count].tripOneArray.join(', ')}.`);
+    console.log(`Change at Union Square to the ${this.trips[count].lineTo} line.`);
+    console.log(`Your journey continues through the following stops: ${this.trips[count].tripTwoArray.join(', ')}.`);
+    console.log(`${this.trips[count].stops} stops in total.`);
   }
 };
 
 trainNetwork.planTrip('M', 'Times Square', '6', '33rd');
 trainNetwork.planTrip('L', '8th', '6', '33rd');
 trainNetwork.planTrip('L', '8th', 'L', '3rd');
+trainNetwork.planTrip('6', '9th', 'L', '3rd');

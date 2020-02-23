5577b2dfb98f995ac6a6588e1a43efedc3517f1b
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-02-23 20:37:58 +1100

    Week02 atm further animation

--- mitchell_browne/week02/atm/js/atm.js
+++ mitchell_browne/week02/atm/js/atm.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 
-//place border line in middle, with GA bank logo in circle in center. keep 100% opacity
+
 
 //make capable to include floats
-//add scroll up to front page with GA logo
+
+// clean up commentary
 
 
 // create object to hold account information
@@ -42,6 +43,8 @@ const deductAmount = function(type, nonType) {
   const accountNonType = `${nonType}_amount`;
   const balance = `#${type}_balance`;
   const balanceNonType = `#${nonType}_balance`;
+  const input_message = `#${type}_message`
+  const animation = 'animated flash';
   if ($(input).val() > 0) {
     const amount = parseInt($(input).val());
     if (account[accountType] >= amount) {
@@ -58,7 +61,12 @@ const deductAmount = function(type, nonType) {
       $(balanceNonType).html(`$${account[accountNonType]}`).addClass('animated flash').one('animationend', function() {
         $(balanceNonType).removeClass('animated flash');
       });
-    }
+    } else if (account[accountType] + account[accountNonType] <= amount) {
+      $(input_message).css('visibility', 'visible').addClass(animation).one('animationend', function() {
+        $(balance).removeClass(animation);
+        $(input_message).css('visibility', 'hidden');
+      });
+    };
     if (account[accountNonType] === 0) {
       $(balanceNonType).addClass('zero_balance');
     }
@@ -69,30 +77,49 @@ const deductAmount = function(type, nonType) {
   }
 }
 
+//BUTTONS
+
 // checking deposit button
 $('#checking_deposit').on('click', function() {
   addAmount('checking');
   pimpMa();
+}).on('mousedown', function() {
+  $(this).addClass('pressed_button');
+}).on('mouseup', function() {
+  $(this).removeClass('pressed_button');
 });
 
 // checking withdraw button
 $('#checking_withdraw').on('click', function() {
   deductAmount('checking','savings');
   pimpMa();
+}).on('mousedown', function() {
+  $(this).addClass('pressed_button');
+}).on('mouseup', function() {
+  $(this).removeClass('pressed_button');
 });
 
 // savings deposit button
 $('#savings_deposit').on('click', function() {
   addAmount('savings');
   pimpMa();
+}).on('mousedown', function() {
+  $(this).addClass('pressed_button');
+}).on('mouseup', function() {
+  $(this).removeClass('pressed_button');
 });
 
 // savings withdraw button
 $('#savings_withdraw').on('click', function() {
   deductAmount('savings','checking');
   pimpMa();
+}).on('mousedown', function() {
+  $(this).addClass('pressed_button');
+}).on('mouseup', function() {
+  $(this).removeClass('pressed_button');
 });
 
+//Input box update with placeholder upon click
 
 // change input box look upon click
 $('#checking_amount').click(function(e) {
@@ -134,7 +161,30 @@ $savings_content.on('mouseenter', function() {
 });
 
 
+// scroll effect to move middle border line and bring opacity of main page up
+$(document).ready(function() {
 
+  let $w = $(window);
+  let $line = $('.line');
+  let lastScrollTop = $w.scrollTop();
+  let _y = 0;
+
+  $w.scroll(function(event) {
+    let st = $w.scrollTop();
+    let bottom = ((st / $w.height())-1)*200;
+    let opacity = (st / $w.height())-0.1;
+    let shadow = (st / $w.height())*10;
+    let box_shadow = (st / $w.height())*1;
+    let rotate = (st / $w.height())*390;
+    console.log(st);
+    lastScrollTop = st;
+    $line.css({'bottom': bottom+'vh', 'opacity': opacity});
+    $('.title').css('text-shadow', `${shadow}px ${shadow}px 0 #000,0px 0px 0 #000`);
+    $('.balance').css('box-shadow', `${box_shadow}mm ${box_shadow}mm 0 #000`);
+    $('#container').css('opacity', opacity);
+    $('#logo').css('transform', `rotate(${rotate}deg)`);
+  });
+})
 
 
 

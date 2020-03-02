0adcd547d54c6a5c1f36a82d3492be2f8271cb10
Yukiko <yukikogimi@gmail.com>
2020-03-02 20:47:13 +1100

    homework updated

--- yukiko_isogimi/Week_04/Day_01/ruby_homework/calc.rb
+++ yukiko_isogimi/Week_04/Day_01/ruby_homework/calc.rb
@@ -5,6 +5,10 @@ def show_menu
   puts "-=" * 40
   puts "[a] --Addition"
   puts "[s] --Subtraction"
+  puts "[m] --Multiplification"
+  puts "[d] --Divisition"
+  puts "[e] --Exponents"
+  puts "[sq] --Square Roots"
   puts "[q] --Quit"
   print "Enter your Choice: "
 end
@@ -19,30 +23,76 @@ menu_choice = gets.chomp.downcase
 
 def addition
   print "what would you like to add number?:  "
-  number_of_choice1 = gets.to_i
-  print "what would you like to add number?:  "
-  number_of_choice2 = gets.to_i
-  number_of_choice1 + number_of_choice2
+  choice1 = gets.to_i
+  print "What would like to add number?: "
+  choice2 = gets.to_i
+  choice1 + choice2
+
 end
 
 def subtraction
   print "what would you like to subtract number?:  "
-  number_of_choice1 = gets.to_i
-  print "what would you like to subtract number?:  "
-  number_of_choice2 = gets.to_i
-  number_of_choice1 - number_of_choice2
+  choice1 = gets.to_i
+  print "what would you like to subtract number?: "
+  choice2 = gets.to_i
+  choice1 - choice2
+end
+
+def multiplification
+  print "what would you like for number?:  "
+  choice1 = gets.to_i
+  print "what would you like for number?: "
+  choice2 = gets.to_i
+  choice1 * choice2
+end
+
+def division
+  print "what would you like for number?:  "
+  choice1 = gets.to_i
+  choice1 ** choice1
+
+end
+
+def exponents
+  print "what would you like for number?:  "
+  choice1 = gets.to_i
+  print "what would you like for number?: "
+  choice2 = gets.to_i
+  choice1 ** choice2
+end
+
+def square_roots
+  print "what would you like to square root?:  "
+  choice1 = gets.to_i
+  Math.sqrt(choice1)
 end
 
 
+
 until menu_choice == "q"
+
   case menu_choice
   when 'a'
     result = addition
     puts "The result is #{ result }"
 
-
   when 's'
     result = subtraction
+    puts "The result is #{ result }"
+
+  when 'm'
+    result = multiplification
+    puts "The result is #{ result }"
+
+  when 'd'
+    result = division
+    puts "The result is #{ result }"
+
+  when 'e'
+    result = exponents
+    puts "The result is #{ result }"
+  when 'sq'
+    result = square_roots
     puts "The result is #{ result }"
 
   else
@@ -50,7 +100,6 @@ until menu_choice == "q"
 
   end
 
-  puts result
   show_menu
   menu_choice = gets.chomp.downcase
 
---
+++ yukiko_isogimi/Week_04/Day_01/ruby_homework/mortgage_calc.rb
@@ -0,0 +1,10 @@
+# # Bonus
+# ## Mortgage Calculator
+# Calculate the monthly required payment given the other variables as input (look up the necessary variables)
+#
+
+
+print "How much is your property?"
+price = gets.chomp.to_i
+print "How long are you going to pay?"
+length = gets.chomp.to_i

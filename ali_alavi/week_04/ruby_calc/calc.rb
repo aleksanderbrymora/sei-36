1427a97f39ed3e36895ae9c4380d3c1c9471183c
doubleInc <alialavi@gmail.com>
2020-03-02 15:12:43 +1100

    Setup ruby hw folder.

---
+++ ali_alavi/week_04/ruby_calc/calc.rb
@@ -0,0 +1,18 @@
+#menu choice
+#def show_menu
+#   puts "Calc" # look up .center
+#   a - addition
+#   print "enter choice"
+#end
+
+#show_menu
+#menu_choice = gets.chomp.downcase
+
+#until menu_choice == 'q'
+    #case 
+    #when 'a'
+    #    add
+    #end case
+    #print next choice
+    #menu_choice = gets.chomp.downcase
+#end

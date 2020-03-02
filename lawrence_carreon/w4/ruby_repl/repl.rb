a5dfda352a3e3ff39ea74e8a0d3e33e015b1aebf
Lawrence Carreon <l.carreon123@gmail.com>
2020-03-03 10:59:34 +1100

    corrected some bugs with 'history' in the repl file

--- lawrence_carreon/w4/ruby_repl/repl.rb
+++ lawrence_carreon/w4/ruby_repl/repl.rb
@@ -1,22 +1,23 @@
 require "readline"  # allows the repl to cycle through previous inputs and have a prompt symbol display while the file is running
 require "coderay" # making our outputs look pretty
 require "pp" # formats outputs in a more presentable manner - required to apply pretty_inspect method on an object
+require "pry"
 
 session_bind = binding # create a variable to store the information of the current running session
 
 while input = Readline.readline("> ", true) # so the file doesn't crash on an invalid input
     # a basic error handling structure
-    history = puts Readline::HISTORY.to_a
+    history = Readline::HISTORY.to_a
     begin
         result = session_bind.eval input
     rescue StandardError => error
         puts "\e[31m #{error.class}:\e[0m #{error.message}" # using some unicode to color a certain part of the text, otherwise it is just a regular string utlilsiing stored variables
     else
         case input
-        when history
-            next
-        # allows the user to read all typed inputs for the current session
-        else puts CodeRay.encode(result.pretty_inspect, :ruby, :terminal)
+        when "history"
+            puts history # allows the user to read all typed inputs for the current session
+        else
+            puts CodeRay.encode(result.pretty_inspect, :ruby, :terminal)
         end
     end
 end

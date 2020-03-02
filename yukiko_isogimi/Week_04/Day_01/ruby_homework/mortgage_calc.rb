86d1c99249c248bb704a22d88c1e69d68a636199
Yukiko <yukikogimi@gmail.com>
2020-03-02 21:08:26 +1100

    homework_last_version

--- yukiko_isogimi/Week_04/Day_01/ruby_homework/mortgage_calc.rb
+++ yukiko_isogimi/Week_04/Day_01/ruby_homework/mortgage_calc.rb
@@ -4,7 +4,13 @@
 #
 
 
-print "How much is your property?"
+print "How much is your property?: "
 price = gets.chomp.to_i
-print "How long are you going to pay?"
-length = gets.chomp.to_i
+print "How many years do you need to pay up?: "
+length = gets.chomp.to_i * 12
+print "What is the annual interest?: "
+interest = gets.chomp.to_i / 12
+
+mortage = (price / length) + interest
+
+print "Your monthly mortgage is #{ mortage }"

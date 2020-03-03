cead1e3adf191c0e47bd3d62c205b64192734453
Lawrence Carreon <l.carreon123@gmail.com>
2020-03-04 09:54:20 +1100

    improved one-liner and readable version

--- lawrence_carreon/w4/ruby_mta/mta.rb
+++ lawrence_carreon/w4/ruby_mta/mta.rb
@@ -1,4 +1,4 @@
-require "colorize"
+require "colorize"; require "pry"
 
 @stations = { 
     "N" => ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"],
@@ -6,11 +6,11 @@ require "colorize"
     "6" => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"],
 }
 def plan_trip start_line, start_station, end_line, end_station
-    puts "Travel on the #{start_line} line for the following stops: "
-    stops = (@stations[start_line].index start_station) < @stations[start_line].index("Union Square") ? [@stations[start_line][((@stations[start_line].index start_station) + 1)..(@stations[start_line].index("Union Square"))]] : [@stations[start_line][(@stations[start_line].index start_station)..(@stations[start_line].index("Union Square") - 1)].reverse]
-    stops.push(["Change at Union Square and continue along the #{end_line}: ".light_magenta]) if start_line != end_line
-    stops.push((@stations[end_line].index end_station) < @stations[end_line].index("Union Square") ? [@stations[end_line][(@stations[end_line].index end_station)..(@stations[end_line].index("Union Square") - 1)].reverse] : [@stations[end_line][(@stations[start_line].index("Union Square") + 1)..(@stations[start_line].index end_station)]])
-    puts stops.flatten!
+    puts "Travel on the #{start_line} line for the following stops: ".green
+    stops = ((@stations[start_line].index start_station) < @stations[start_line].index("Union Square") ? [@stations[start_line][((@stations[start_line].index start_station) + 1)..(@stations[start_line].index("Union Square"))]] : [@stations[start_line][(@stations[start_line].index start_station)..(@stations[start_line].index("Union Square") - 1)].reverse])
+    stops.push(["Change at Union Square and continue along the #{end_line}: ".light_magenta]) if start_line != end_line && end_station != "Union Square"
+    puts stops.push(end_station == "Union Square" ? [" "] : ((@stations[end_line].index end_station) < @stations[end_line].index("Union Square") ? [@stations[end_line][(@stations[end_line].index end_station)..(@stations[end_line].index("Union Square") - 1)].reverse] : [@stations[end_line][(@stations[end_line].index("Union Square") + 1)..(@stations[end_line].index end_station)]])).flatten!
+    puts "Arrive at #{end_station} on the #{end_line} line".green
     puts "Total stops: #{start_line == end_line ? stops.size : (stops.size - 1)}".green
     puts "Thanks for using MTA!\n #{("-=" * 40)}".light_blue
 end

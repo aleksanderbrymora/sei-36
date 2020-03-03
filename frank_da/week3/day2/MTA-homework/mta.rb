8e9cc524dfef9603352c84b8c03bd0d25ff7487a
frankda <suyunyi@gmail.com>
2020-03-03 19:46:51 +1100

    Final homework

--- frank_da/week3/day2/MTA-homework/mta.rb
+++ frank_da/week3/day2/MTA-homework/mta.rb
@@ -1,12 +1,5 @@
 require 'pry'
 
-mta = {
-    :N => ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'],
-    :L => ['8th', '6th', 'Union Square', '3rd', '1st'],
-    :'6' => ['Grand Central', '33rd', '28th', '23rd', 'Union Square', 'Astor Place']
-}
-
-
 def plan_trip start_line, start_stop, end_line, end_stop
     mta = {
         :N => ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'],

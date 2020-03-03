f7452a6ac6dd96e0c4c0d2635122a4158ab3bb93
Lawrence Carreon <l.carreon123@gmail.com>
2020-03-04 09:20:15 +1100

    compressed the readable version of mta

--- lawrence_carreon/w4/ruby_mta/mta.rb
+++ lawrence_carreon/w4/ruby_mta/mta.rb
@@ -1,34 +1,22 @@
 require "colorize"
 
-@stations = {
+@stations = { 
     "N" => ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"],
     "L" => ["8th", "6th", "Union Square", "3rd", "1st"],
     "6" => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"],
-    :stops => 0
 }
 def plan_trip start_line, start_station, end_line, end_station
-    @stations[:stops] = 0
-    if start_line == end_line
-        describe_trip start_line, start_station, end_station, end_station
-    else
-        describe_trip start_line, start_station, "Union Square", end_station
-        describe_trip end_line, "Union Square", end_station, end_station
-    end
-    puts "Total stops: #{@stations[:stops]}".green
-    puts "Thanks for using MTA!".light_blue
-    puts ("-=" * 40).light_blue
-end
-def describe_trip line, start, destination, end_trip
-    index_of_boarding, index_of_alight = @stations[line].index(start), @stations[line].index(destination)
-    puts "Travel on the #{line} line for the following stops:".green
-    stops = index_of_boarding > index_of_alight ? @stations[line][index_of_alight..index_of_boarding - 1].reverse : @stations[line][index_of_boarding + 1..index_of_alight]
-    puts stops
-    @stations[:stops] += stops.size
-    puts end_trip != "Union Square" && destination == "Union Square" ? "Change at Union Square.".light_magenta : "Arrive at #{destination}.".green
+    puts "Travel on the #{start_line} line for the following stops: "
+    stops = (@stations[start_line].index start_station) < @stations[start_line].index("Union Square") ? [@stations[start_line][((@stations[start_line].index start_station) + 1)..(@stations[start_line].index("Union Square"))]] : [@stations[start_line][(@stations[start_line].index start_station)..(@stations[start_line].index("Union Square") - 1)].reverse]
+    stops.push(["Change at Union Square and continue along the #{end_line}: ".light_magenta]) if start_line != end_line
+    stops.push((@stations[end_line].index end_station) < @stations[end_line].index("Union Square") ? [@stations[end_line][(@stations[end_line].index end_station)..(@stations[end_line].index("Union Square") - 1)].reverse] : [@stations[end_line][(@stations[start_line].index("Union Square") + 1)..(@stations[start_line].index end_station)]])
+    puts stops.flatten!
+    puts "Total stops: #{start_line == end_line ? stops.size : (stops.size - 1)}".green
+    puts "Thanks for using MTA!\n #{("-=" * 40)}".light_blue
 end
 def user_inputs question_string
     puts "Which #{question_string} at?".light_blue
-    output = gets.chomp
+    gets.chomp
 end
 loop do
     puts "Welcome to MTA!".light_green

6764afe670565e64c632940629da6b1acee68b98
Lawrence Carreon <l.carreon123@gmail.com>
2020-03-04 10:15:29 +1100

    added start location check

--- lawrence_carreon/w4/ruby_mta/mta.rb
+++ lawrence_carreon/w4/ruby_mta/mta.rb
@@ -1,4 +1,4 @@
-require "colorize"; require "pry"
+require "colorize"
 
 @stations = { 
     "N" => ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"],
@@ -6,6 +6,7 @@ require "colorize"; require "pry"
     "6" => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"],
 }
 def plan_trip start_line, start_station, end_line, end_station
+    return puts "You are already at #{start_station}".red if start_station == end_station && start_line == end_line
     puts "Travel on the #{start_line} line for the following stops: ".green
     stops = ((@stations[start_line].index start_station) < @stations[start_line].index("Union Square") ? [@stations[start_line][((@stations[start_line].index start_station) + 1)..(@stations[start_line].index("Union Square"))]] : [@stations[start_line][(@stations[start_line].index start_station)..(@stations[start_line].index("Union Square") - 1)].reverse])
     stops.push(["Change at Union Square and continue along the #{end_line}: ".light_magenta]) if start_line != end_line && end_station != "Union Square"

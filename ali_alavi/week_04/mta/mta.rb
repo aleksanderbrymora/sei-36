1c3f3762248db466b42a4dd577971c2ddea355c2
doubleInc <aalavi@protonmail.com>
2020-03-03 22:37:46 +1100

    Finished mta in ruby.

--- ali_alavi/week_04/mta/mta.rb
+++ ali_alavi/week_04/mta/mta.rb
@@ -24,14 +24,28 @@ def find_in_route(s, sLine, e, eLine)
     if sLine == eLine
         #save line
         line = Lines[sLine]
-        return along_same_line(s, e, line)
+        return "You must travel through the following stops on the #{sLine} line: #{along_same_line(s, e, line).join(", ")}"
     else
         #if different lines
         trip1 = along_same_line(s, "Union Square", Lines[sLine])
         trip2 = along_same_line("Union Square", e, Lines[eLine])
-        return trip1 + trip2
+        return "You must travel through the following stops on the #{sLine} line: #{trip1.join(", ")}\nChange at Union Square and continue on line #{eLine}: #{trip2.join(", ")}"
     end
 end
+
+def trip_planner
+    print "Welcome to the MTA trip planner.\nPlease enter a starting station: \n"
+    start_station = gets.chomp
+    print "Enter the line to start on: "
+    start_line = gets.chomp
+    print "Enter the destination station: "
+    end_station = gets.chomp
+    print "Enter the destination line: "
+    end_line = gets.chomp
+    print "#{find_in_route(start_station, start_line, end_station , end_line)}"
+end
+
+trip_planner
 
 find_in_route("34th", "N", "23rd", "N") # => ["28th", "23rd"]
 find_in_route("8th", "N", "Times Square", "N") # => ["Union Square", "23rd", "28th", "34th", "Times Square"]

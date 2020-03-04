b9e2c13478c68e43cfcf75939b36330fd1c235ac
Carmina <cvdsoto@gmail.com>
2020-03-04 16:13:39 +1100

    Revised MTA ruby code

--- carmina_soto/week_04/day_02/homework/mta.rb
+++ carmina_soto/week_04/day_02/homework/mta.rb
@@ -16,10 +16,12 @@ show_menu
 menu_choice = gets.chomp.downcase
 
 # def get_subway_line line
-#   subway = $n_subway_line if line == "N"
-#   subway = $l_subway_line if line == "L"
-#   subway = $six_subway_line if line == "6"
+  # subway = []
+  # subway = $n_subway_line if line == "N"
+  # subway = $l_subway_line if line == "L"
+  # subway = $six_subway_line if line == "6"
 # end
+
 def move_subway start_stop, end_stop, line
   if start_stop > end_stop
     subway_stops = line[end_stop..start_stop - 1].reverse
@@ -43,17 +45,17 @@ def plan_trip start_line, start_stop, end_line, end_stop
   if start_subway == end_subway
     stops = move_subway start_subway.index(start_stop), start_subway.index(end_stop), start_subway
     no_of_stops = stops.count
-    puts Rainbow("You must travel through the following stops on the #{start_line} line: #{stops.join(', ')}
-    #{no_of_stops} stops in total").purple
+    puts Rainbow("You must travel through the following stops on the #{start_line} line: #{stops.join(', ')}").purple
+    puts Rainbow("#{no_of_stops} stops in total").purple
   else
     start_stops = move_subway start_subway.index(start_stop), start_subway.index("Union Square"), start_subway
     end_stops = move_subway end_subway.index("Union Square"), end_subway.index(end_stop), end_subway
     stops = start_stops + end_stops
     no_of_stops = stops.count
-    puts Rainbow("You must travel through the following stops on the #{start_line} line: #{start_stops.join(', ')}
-    Change at Union Square
-    Your journey continues through the following stops on the #{end_line} line: #{end_stops.join(', ')}
-    #{no_of_stops} stops in total").purple
+    puts Rainbow("You must travel through the following stops on the #{start_line} line: #{start_stops.join(', ')}").purple
+    puts Rainbow("Change at Union Square").purple
+    puts Rainbow("Your journey continues through the following stops on the #{end_line} line: #{end_stops.join(', ')}").purple
+    puts Rainbow("#{no_of_stops} stops in total").purple
   end
 end
 
@@ -63,11 +65,11 @@ until menu_choice == 'q'
   puts Rainbow("L Line: 8th, 6th, Union Square, 3rd, 1st").green
   puts Rainbow("6 Line: Grand Central, 33rd, 28th, 23rd, Union Square, Astor Place").aqua
   print "Enter start line: "
-  start_line_mta = gets.chomp
+  start_line_mta = gets.chomp.upcase
   print "Enter start stop: "
   start_stop_mta = gets.chomp
   print "Enter end line: "
-  end_line_mta = gets.chomp
+  end_line_mta = gets.chomp.upcase
   print "Enter end stop: "
   end_stop_mta = gets.chomp
   plan_trip start_line_mta, start_stop_mta, end_line_mta, end_stop_mta
@@ -77,9 +79,3 @@ until menu_choice == 'q'
 end
 
 puts "Thanks for using the MTA Trip Planner!"
-
-
-
-# plan_trip "N", "Times Square", "N", "8th"
-# plan_trip "N", "34th", "6", "33rd"
-# plan_trip "L", "1st", "6", "Grand Central"

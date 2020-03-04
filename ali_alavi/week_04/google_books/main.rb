33ef73966c4f3db4ad150c044cd077907a7b42d0
doubleInc <aalavi@protonmail.com>
2020-03-04 23:38:25 +1100

    Json response done.

--- ali_alavi/week_04/google_books/main.rb
+++ ali_alavi/week_04/google_books/main.rb
@@ -1,20 +1,34 @@
-# require 'sinatra'#dsl
-# require 'sinatra/reloader'
-# require 'httparty'
+require 'sinatra'#dsl
+require 'sinatra/reloader'
+require "sinatra/json"
+require 'httparty'
 
+# lens
 require 'xf'
 
-#routes
-# get "/" do
-#     erb :home #embeded ruby 
-# end
+# routes
+get "/" do
+    erb :home #embeded ruby 
+end
 
 #lenses
-people = [{name: "Robert", age: 22}, {name: "Roberta", age: 22}, {name: "Foo", age: 42}, {name: "Bar", age: 18}]
+# people = [{name: "Robert", "age": 22}, {name: "Roberta", "age": 22}, {name: "Foo", "age": 42}, {name: "Bar", age: 18}]
 
-people.map(&Xf.scope(:age).get) # => [22, 22, 42, 18]
+# people.map(&Xf.scope(:age).get) # => [22, 22, 42, 18]
 
 #require httparty
 #@bookurl ="bookslink#{usr_input}"
 #@book_info = "HTTParty.get @book"
 #@book_info["items"].first["volumeInfo"]["imageLinks"]["thumbnail"]
+
+#routes
+get "/result" do
+    begin
+        @book = params[:bookname]
+        @book_info = HTTParty.get "https://www.googleapis.com/books/v1/volumes?q=title:#{@book}"
+    rescue
+
+    end
+    @cover_url = @book_info["items"].first["volumeInfo"]["imageLinks"]["thumbnail"]
+    json :result => @cover_url #embeded ruby 
+end

d1a4b34a104806d12548f05a77a298d987464af1
Lawrence Carreon <l.carreon123@gmail.com>
2020-03-04 22:18:12 +1100

    compressed the pair_programming main.rb code a bit

--- lawrence_carreon/w4/pair_programming/main.rb
+++ lawrence_carreon/w4/pair_programming/main.rb
@@ -2,46 +2,36 @@ require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-    @question = "Do you have a test for that?"
-    @yes_route = "/pass"
-    @no_route = "/write_test"
+    @question, @yes_route, @no_route = "Do you have a test for that?", "/pass", "/write_test"
     erb :question
 end
 
 get "/pass" do
-    @question = "Does the test pass?"
-    @yes_route = "/refactor"
-    @no_route = "/write_code"
+    @question, @yes_route, @no_route = "Does the test pass?", "/refactor", "/write_code"
     erb :question
 end
 
 get "/refactor" do
-    @question = "Need to refactor?"
-    @yes_route = "/do_refactor"
-    @no_route = "/new_feature"
+    @question, @yes_route, @no_route = "Need to refactor?", "/do_refactor", "/new_feature"
     erb :question
 end
 
 get "/write_code" do
-    @command = "Write just enough code for the test to pass."
-    @complete_route = "/pass"
+    @command, @complete_route = "Write just enough code for the test to pass.", "/pass"
     erb :instruction
 end
 
 get "/write_test" do
-    @command = "Write a test."
-    @complete_route = "/pass"
+    @command, @complete_route = "Write a test.", "/pass"
     erb :instruction
 end
 
 get "/do_refactor" do
-    @command = "Refactor the code."
-    @complete_route = "/pass"
+    @command, @complete_route = "Refactor the code.", "/pass"
     erb :instruction
 end
 
 get "/new_feature" do
-    @command = "Select a new feature to implement"
-    @complete_route = "/"
+    @command, @complete_route = "Select a new feature to implement", "/"
     erb :instruction
 end

8df53f8dec9097488e9ed68bf83da695b888de0d
Carmina <cvdsoto@gmail.com>
2020-03-08 16:55:58 +1100

    Revised main.rb for Active Record

--- carmina_soto/week_04/day_04/homework/formula1/main.rb
+++ carmina_soto/week_04/day_04/homework/formula1/main.rb
@@ -1,13 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => 'database.sqlite3'
+)
+
+ActiveRecord::Base.logger = Logger.new(STDERR)
+
+#Model: class + database table
+class Driver < ActiveRecord::Base
+end
+
+class Team < ActiveRecord::Base
+end
 
 get '/' do
   erb :home
 end
 
 get '/drivers' do
-  @drivers = query_db "SELECT * FROM drivers"
+  @drivers = Driver.all
   erb :drivers_index
 end
 
@@ -16,61 +31,51 @@ get '/drivers/new' do
 end
 
 post '/drivers' do
-    query_db "INSERT INTO drivers (name, team, podiums, image) VALUES ('#{params[:name]}', '#{params[:team]}', '#{params[:podiums]}', '#{params[:image]}')"
+    driver = Driver.new
+    driver.name = params[:name]
+    driver.team = params[:team]
+    driver.podiums = params[:podiums]
+    driver.image = params[:image]
+    driver.save
     redirect to('/drivers')
 end
 
 get '/drivers/:id' do
-  @driver = query_db "SELECT * FROM drivers WHERE id='#{params[:id]}'"
-  @driver = @driver.first
-  @team = query_db_team "SELECT * FROM teams WHERE name='#{@driver["team"]}'"
-  @team = @team.first
+  @driver = Driver.find params[:id]
   erb :drivers_show
 end
 
 get '/drivers/:id/edit' do
-  @driver = query_db "SELECT * FROM drivers WHERE id='#{params[:id]}'"
-  @driver = @driver.first
+  @driver = Driver.find params[:id]
   erb :drivers_edit
 end
 
-get '/drivers/:id/delete' do
-  query_db "DELETE FROM drivers WHERE id='#{params[:id]}'"
-  redirect to ('/drivers')
-end
-
 post '/drivers/:id' do
-  query_db "UPDATE drivers SET name='#{params[:name]}', team='#{params[:team]}', podiums='#{params[:podiums]}', image='#{params[:image]}' WHERE id='#{params[:id]}'"
+  driver = Driver.find params[:id]
+  driver.name = params[:name]
+  driver.team = params[:team]
+  driver.podiums = params[:podiums]
+  driver.image = params[:image]
+  driver.save
   redirect to("/drivers/#{params[:id]}")
 end
 
+get '/drivers/:id/delete' do
+  driver = Driver.find params[:id]
+  driver.destroy
+  redirect to ('/drivers')
+end
+
 get '/teams' do
-  @teams = query_db_team "SELECT * FROM teams"
+  @teams = Team.all
   erb :teams_index
 end
 
 get '/teams/:id' do
-    @team = query_db_team "SELECT * FROM teams WHERE id='#{params[:id]}'"
-    @team = @team.first
-    @drivers = query_db "SELECT * FROM drivers WHERE team='#{@team["name"]}'"
+    @team = Team.find params[:id]
     erb :teams_show
 end
 
-
-def query_db(sql_statement)
-  puts sql_statement #Optional but nice for debugging
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.results_as_hash = true
-  results = db.execute sql_statement
-  db.close
-  results #implicitly return
-end
-
-def query_db_team (sql_statement)
-  puts sql_statement #Optional but nice for debugging
-  db = SQLite3::Database.new 'database2.sqlite3'
-  db.results_as_hash = true
-  results = db.execute sql_statement
-  db.close
-  results #implicitly return
+after do
+  ActiveRecord::Base.connection.close
 end

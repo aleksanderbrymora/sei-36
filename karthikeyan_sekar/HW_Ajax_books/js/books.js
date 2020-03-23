914f15c6a2fabd4581b9cfff15c3aeb5b68f04d9
karthikeyansekar <karthikeyansep27@gmail.com>
2020-03-24 00:06:55 +1100

    Google books in Ajax

---
+++ karthikeyan_sekar/HW_Ajax_books/js/books.js
@@ -0,0 +1,26 @@
+const fetchFact = function () {
+  const xhr = new XMLHttpRequest(); // ready state 0
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== 4) {
+      return; // Nothing to do yet so exit the function.
+    }
+
+    // console.log(xhr.responseText);
+
+    const data = JSON.parse( xhr.responseText );
+
+    const p = document.createElement('p');
+    p.innerHTML = data.items[0].volumeInfo.title;
+
+    const q = document.createElement('p');
+    q.innerHTML = data.items[0].volumeInfo.description;
+
+    document.body.appendChild(p);
+    document.body.appendChild(q);
+  };
+  xhr.open('GET', `https://www.googleapis.com/books/v1/volumes?q=title:${document.getElementById('title').value}`); // ready state 1
+  xhr.send(); // Asynchronous // ready state 2 and 3 and 4
+};
+
+document.getElementById('fetch').addEventListener('click', fetchFact);

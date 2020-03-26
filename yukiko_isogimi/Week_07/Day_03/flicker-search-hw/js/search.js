a194434bab782e3e20bcada11c0f14e23fc42182
Yukiko <yukikogimi@gmail.com>
2020-03-26 14:53:14 +1100

    hw w7 d3

--- yukiko_isogimi/Week_07/Day_03/flicker-search-hw/js/search.js
+++ yukiko_isogimi/Week_07/Day_03/flicker-search-hw/js/search.js
@@ -1,83 +1,84 @@
 // console.log($.fn.jquery, _.VERSION);
 
 const state = {
-   currentPage :1,
-   requestInProgress: false
+  currentPage: 1,
+  lastPageReached: false
 };
 
-const generateURL = function (p) {
+const generateURL = function(p) {
   // console.log(p);
   return [
-    'http://farm',
+    "http://farm",
     p.farm,
-    '.static.flickr.com/',
+    ".static.flickr.com/",
     p.server,
-    '/',
+    "/",
     p.id,
-    '_',
+    "_",
     p.secret,
-    '_q.jpg'//change this to something else for different sizes ( see docs )
+    "_q.jpg" //change this to something else for different sizes ( see docs )
+  ].join("");
+};
 
-  ].join('');
-
-}
-
-const showImages = function (results) {
-  console.log( results );
-  _.each(results.photos.photo,function (photo) {
+const showImages = function(results) {
+  console.log(results);
+  _.each(results.photos.photo, function(photo) {
     // console.log(generateURL);
     const thumbnailURL = generateURL(photo);
     // console.log(generateURL);
-    const $img = $('<img>',{src: thumbnailURL});
-    $img.appendTo('#images');
-
+    const $img = $("<img>", { src: thumbnailURL });
+    $img.appendTo("#images");
   });
 };
 
-const searchFlickr = function (words){
-  if (state.requestInProgress === true){
+const searchFlickr = function(words) {
+  if (state.lastPageReached === true) {
     return;
   }
-  console.log('Searching Flickr for', words);
+  console.log("Searching Flickr for", words);
 
-  state.requestInProgress = true;
-
-  const flickrURL = 'https://api.flickr.com/services/rest?jsoncallback=?';
+  const flickrURL = "https://api.flickr.com/services/rest?jsoncallback=?";
   $.getJSON(flickrURL, {
-
-    method: 'flickr.photos.search',
-    api_key: '2f5ac274ecfac5a455f38745704ad084', // This is not a secret key
+    method: "flickr.photos.search",
+    api_key: "2f5ac274ecfac5a455f38745704ad084", // This is not a secret key
     text: words,
     page: state.currentPage++,
-    format: 'json'
-  }).done(showImages).done(function () {
-    state.requestInProgress = false;
-  });
-
+    format: "json"
+  })
+    .done(showImages)
+    .done(function(data) {
+      if (data.photos.page >= data.photos.pages) {
+        state.lastPageReached = true;
+      }
+    });
 };
 
-
-
-$(document).ready(function(){
-  $('form#search').on('submit',function(event){
+$(document).ready(function() {
+  $("form#search").on("submit", function(event) {
     event.preventDefault(); //Dont go anywhere
 
     state.currentPage = 1;
+    state.lastPageReached = false;
+
+    $("#images").empty();
     //find the search term
-    const term = $('#query').val();
-    searchFlickr( term );
+    const term = $("#query").val();
+    searchFlickr(term);
+  });
+
+  const debounceSearchFlickr = _.debounce(searchFlickr, 4000, {
+    training: false
   });
 
   //Extremely twitchy : todo chill out
-  $(window).on('scroll', function () {
+  $(window).on("scroll", function() {
     // console.log('scroll' + Math.random());
-    const scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
+    const scrollBottom =
+      $(document).height() - $(window).height() - $(window).scrollTop();
     if (scrollBottom < 700) {
-      const term = $('#query').val();
+      const term = $("#query").val();
       // currentTerm = term;
-      searchFlickr( term );
+      debounceSearchFlickr(term);
     }
-
   });
-
 });
